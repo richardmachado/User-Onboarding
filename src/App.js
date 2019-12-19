33055289de07827f2c270022b97d41183fc2380c
Lambda_School_Loaner_198 <lambda_school_loaner_198@Lambda-School-Loaner-198s-MacBook-Pro.local>
2019-12-19 14:44:09 -0800

    just need to style more

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import UserForm from "./InputForm"
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className ="App">
+          <div className = "header">
+              <h1 className = "title">Signup Form</h1>
+          </div>
+     
+      <UserForm />
+      
+
+   </div>
+   
   );
-}
+};
 
 export default App;
